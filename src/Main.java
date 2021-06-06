68ecbbb1faa1cb102f306b3d36d933d7ebcc42ee
Kaloyan Kostadinov <kaloqn_kostadinow@abv.bg>
2021-06-06 13:07:26 +0300

    added while loop exercises

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int poorGradesLimit = Integer.parseInt(scanner.nextLine());
+        String problemName = scanner.nextLine();
 
-        String text = scanner.nextLine();
+        while (!problemName.equals("Enough")) {
+            int problemNameNumber = Integer.parseInt(problemName);
+            int grade = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 0; i < text.length(); i++){
-            System.out.println(text.charAt(i));
+
+            problemName = scanner.nextLine();
         }
 
-
     }
+
 }
